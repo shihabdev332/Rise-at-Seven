1ac5ced63efb491951067c0d17137ebc2981c5ab
Shihab <shihab.dev332@gmail.com>
2026-05-12 22:45:33 +0600

    added base path

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
+  // base path যুক্ত করা হয়েছে যাতে GitHub Pages-এ ফাইলগুলো ঠিকঠাক লোড হয়
+  base: '/Rise-at-Seven/', 
+  plugins: [react(), tailwindcss()],
 })
